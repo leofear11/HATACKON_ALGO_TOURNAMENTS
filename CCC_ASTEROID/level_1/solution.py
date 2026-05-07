e1ed62bcc2c3e94156235fee6ccf2528471385d9
RAJO <otsutsukirajo@gmail.com>
2026-05-07 14:20:51 +0300

    Delete CCC_ASTEROID/level_1 directory

--- CCC_ASTEROID/level_1/solution.py
+++
@@ -1,8 +0,0 @@
-# 1. Ouvrir le fichier
-with open("level1_1_small.in", "r") as f:
-    lines = f.readlines()
-
-# 2. Lire le nombre d'éléments (souvent sur la 1ère ligne)
-n = int(lines[0].strip())
-
-print(n)
